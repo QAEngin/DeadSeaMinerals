230a21a7aea493422fd190512e695e0d39f2ec35
QAEngingit config --global user.name <zura332@gmail.com>
2026-09-25 14:19:05 +0300

    Replace with your WhatsApp number in international format without '+' or dashes

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,4 +1,4 @@
-const WHATSAPP_NUMBER = '955591585522'
+const WHATSAPP_NUMBER = '972544343247' // Replace with your WhatsApp number in international format without '+' or dashes
 const STATIC_TEXT = 'გამარჯობათ, დაინტერესებული ვარ ამ პროდუქტით!'
 
 export function buildWhatsAppLink(product) {
